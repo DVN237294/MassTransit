aec9447db26ee4beec895915a79925ca7e6d9f5f
David Nielsen <david.nielsen@bestseller.com>
2023-02-22 08:56:37 +0100

    add a bit more doc

--- src/MassTransit.Abstractions/JobService/Contracts/JobService/JobSubmitted.cs
+++ src/MassTransit.Abstractions/JobService/Contracts/JobService/JobSubmitted.cs
@@ -24,7 +24,8 @@ namespace MassTransit.Contracts.JobService
         TimeSpan JobTimeout { get; }
 
         /// <summary>
-        /// An optional concurrency limitation key. Job types with the same key will not run concurrently 
+        /// An optional concurrency limitation key. Job types with the same key will not run concurrently.
+        /// The key should be as short as possible while still satisfying any uniqueness requirements
         /// </summary>
         string ConcurrencyKey { get; }
 
